c9f9357eb19bd3806e55d522edc6705c0587a3e4
Sean Glancy <spglancy@gmail.com>
2020-04-23 11:20:05 -0700

    prob 2

--- problem2.py
+++ problem2.py
@@ -1,6 +1,16 @@
 # Given an array a of n numbers and a count k find the k largest values in the array a.
-def klargest(nums, k):
+def klargest1(nums, k):
   nums.sort(reverse=True)
-  return nums.slice(0,k)
+  output = []
+  for i in range(k):
+    output.append(nums[i])
+  return output
+
+def klargest(nums, k):
+  nums.sort()
+  output = []
+  for i in range(k):
+    output.append(nums[len(nums)-i-1])
+  return output
 
 print(klargest([1,2,3,4,5,6,7,8], 3))
